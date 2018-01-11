59c668d47e3535a6fa2a9f4a6eeed5921668d899
Andy Liu <isbner@gmail.com>
2018-01-11 12:30:31 +0800

    Update stega.py

--- stega.py
+++ stega.py
@@ -2,7 +2,7 @@
 # import section
 from __future__ import absolute_import, unicode_literals
 from steganography.steganography import Steganography
-from Crypto.Cipher import AES
+# from Crypto.Cipher import AES
 from binascii import b2a_hex, a2b_hex, b2a_base64, a2b_base64
 import sys
 import os
@@ -28,7 +28,7 @@ from difflib import unified_diff
 
 
 def str2bool(v):
-    return str(v).lower() in ("yes", "true", "t", "y", "1"
+    return str(v).lower() in ("yes", "true", "t", "y", "1")
 
 
 def stegano_image(ifile_name, ofile_name, text):
@@ -51,40 +51,6 @@ def convert_text_to_binary(text):
     data = a2b_base64(text)
     return data
 
-
-class prpcrypt():
-    def __init__(self, key):
-        self.key = key
-        self.mode = AES.MODE_CBC
-
-    # 加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数
-    def encrypt(self, text):
-        cryptor = AES.new(self.key, self.mode, self.key)
-        # 这里密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
-        length = 16
-        count = len(text)
-        add = length - (count % length)
-        text = text + ('\0' * add)
-        self.ciphertext = cryptor.encrypt(text)
-        # 因为AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题
-        # 所以这里统一把加密后的字符串转化为16进制字符串
-        return b2a_hex(self.ciphertext)
-
-    # 解密后，去掉补足的空格用strip() 去掉
-    def decrypt(self, text):
-        cryptor = AES.new(self.key, self.mode, self.key)
-        plain_text = cryptor.decrypt(a2b_hex(text))
-        return plain_text.rstrip('\0')
-
-
-def crypt():
-    pc = prpcrypt('keyskeyskeyskeys')  # 初始化密钥
-    e = pc.encrypt("00000")
-    d = pc.decrypt(e)
-    print e, d
-    e = pc.encrypt("00000000000000000000000000")
-    d = pc.decrypt(e)
-    print e, d
 
 
 ##################################
@@ -110,6 +76,7 @@ def main() :
     input_file = vars(args)['input_file']
     output_file = vars(args)['output_file']
     image_filename = vars(args)['image_filename']
+    verbose_mode = True
 
     ret = 0
     if post_to_cloud is True:
@@ -125,11 +92,18 @@ def main() :
             # todo: better to zip ifile before base64 it
             with open(input_file, 'rb') as ifile:
                 data = ifile.read()
+                print "Successfully read input file as ", input_file
+
             text = convert_binary_to_text(data)
-            tempfile_name = os.path.join(os.path.basename(input_file), 'tmp.tmp')
+            print "Successfully convert data to text."
+
+            print "Starting stega image ", image_filename
+            tempfile_name = os.path.join(os.path.dirname(input_file), 'tmp.tmp')
             stegano_image(image_filename, tempfile_name, text)
+            print "Successfully stega image and output as ", tempfile_name
+
             # todo: send this file to cloud
-            print "Successfully stegano a file as image %s", tempfile_name
+            print "Successfully stegano a file as image ", tempfile_name
 
     if fetch_from_cloud is True:
         if output_file is None:
@@ -138,8 +112,14 @@ def main() :
         else:
             # todo: fetch image from cloud
             # todo: remove this image_filename here, we use it as tricky workaround
+
+            print "Starting destega imagefile ", image_filename
             text = destegano_image(image_filename)
+            print "Successfully destega image."
+
             data = convert_text_to_binary(text)
+            print "Successfully convert text to data."
+
             with open(output_file, 'wb') as ofile:
                 ofile.write(data)
             print "Successfully destegano a file as %s", output_file
@@ -150,3 +130,4 @@ def main() :
 if __name__ == '__main__':
     ret = main()
     exit(ret)
+
